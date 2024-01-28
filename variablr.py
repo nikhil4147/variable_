8b356239012a6831a45bde04c26f778ef4e76f9b
Nikhil4147 <nikhil.waghmare4147@gmail.com>
2024-01-28 15:14:21 +0530

    pass commit message here

---
+++ variablr.py
@@ -0,0 +1,22 @@
+#Many Values to Multiple Variables
+
+#Python allows you to assign values to multiple variables in one line:
+
+x,y,z = "cricket", "football","kabbadi"
+print(x)
+print(y)
+print(z)
+print(type(x))
+print(type(y))
+print(type(z))
+
+
+#One Value to Multiple Variables
+
+#And you can assign the same value to multiple variables in one line:
+
+
+A = B = C = "Apple"
+print(A)
+print(B)
+print(C)
